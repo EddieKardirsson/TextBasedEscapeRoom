863f61f3f30235d8fc96770fd7cd4fd5d3872431
Edison Kardirsson <ed.kardirsson@hotmail.com>
2023-12-14 05:35:12 +0100

    Room class

--- main.py
+++ main.py
@@ -1,21 +1,49 @@
 class GameObject:
-
+    # Initialize properties for name, appearance, feel and smell
     def __init__(self, name, appearance, feel, smell):
         self.name = name
         self.appearance = appearance
         self.feel = feel
         self.smell = smell
 
+    # Return string describing the object appearance
     def look(self):
         return f"You look at the {self.name}. {self.appearance}\n"
 
+    # Return string describing object feel
     def touch(self):
         return f"You touch the {self.name}. {self.feel}\n"
 
+    # Return string describing object smell
     def sniff(self):
         return f"You smell on the {self.name}. {self.smell}\n"
 
 
-game_object = GameObject("Knife", "Some appearance", "Some feel", "Some smell")
+class Room:
+    # Room class with an escape code and a list of game objects as member variables
+    def __init__(self, escape_code, game_objects=[]):
+        self.escape_code = escape_code
+        self.game_objects = game_objects
 
-print(game_object.sniff())
+    # Returns if the code player enters match with the escape code
+    def check_code(self, code):
+        return self.escape_code == code
+
+    # Returns a list of the names of the game objects existing inside the room
+    def get_game_object_names(self):
+        names = []
+        for obj in self.game_objects:
+            names.append(obj.name)
+        return names
+
+
+obj_knife = GameObject("Knife", "It looks sharp", "It feels light", "It smells like metal")
+obj_cardbox = GameObject("Cardbox", "It looks like a generic cardbox", "It feels soft", "It smells like shit")
+
+game_objects = [obj_knife, obj_cardbox]
+
+room = Room(384, game_objects)
+
+print(room.get_game_object_names())
+print(room.check_code(666))
+print(room.check_code(384))
