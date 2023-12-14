fc9857e1b44fb0959d42654cf1e98a9e39cf3d6a
Edison Kardirsson <ed.kardirsson@hotmail.com>
2023-12-14 03:17:01 +0100

    GameObject implementation

---
+++ main.py
@@ -0,0 +1,8 @@
+class GameObject:
+
+    def __init__(self, name, appearance, feel, smell):
+        self.name = name
+        self.appearance = appearance
+        self.feel = feel
+        self.smell = smell
+
