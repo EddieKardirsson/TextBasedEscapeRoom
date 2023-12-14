8c8c65389f2010de254d07115f7a050eaca1a90e
Edison Kardirsson <ed.kardirsson@hotmail.com>
2023-12-14 03:42:45 +0100

    Member functions added to GameObject

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ class GameObject:
         self.feel = feel
         self.smell = smell
 
+    def look(self):
+        return f"You look at the {self.name}. {self.appearance}\n"
+
+    def touch(self):
+        return f"You touch the {self.name}. {self.feel}\n"
+
+    def sniff(self):
+        return f"You smell on the {self.name}. {self.smell}\n"
+
+
+game_object = GameObject("Knife", "Some appearance", "Some feel", "Some smell")
+
+print(game_object.sniff())
